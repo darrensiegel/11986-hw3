f0cce2bc8f95323d17c2ab8287970ca338aeee71
Darren Siegel <siegel.darren@gmail.com>
2025-09-18 20:25:35 -0400

    update

--- minitorch/modules_basic.py
+++ minitorch/modules_basic.py
@@ -5,6 +5,7 @@ Sequential
 Embedding
 
 """
+import math
 import numpy as np
 
 from .module import Module, Parameter
@@ -33,7 +34,9 @@ class Embedding(Module):
         self.num_embeddings = num_embeddings # Vocab size
         self.embedding_dim  = embedding_dim  # Embedding Dimension
         ### BEGIN ASSIGN3_2
-        raise NotImplementedError
+        weight_init = np.random.randn(num_embeddings, embedding_dim).astype(np.float32)
+        weight_tensor = tensor_from_numpy(weight_init, backend=backend, requires_grad=True)
+        self.weights = Parameter(weight_tensor)
         ### END ASSIGN3_2
     
     def forward(self, x: Tensor):
@@ -47,7 +50,10 @@ class Embedding(Module):
         """
         bs, seq_len = x.shape
         ### BEGIN ASSIGN3_2
-        raise NotImplementedError
+        one_hot_x = one_hot(x, self.num_embeddings)
+        flat = one_hot_x.contiguous().view(bs * seq_len, self.num_embeddings)
+        embeddings = flat @ self.weights.value
+        return embeddings.view(bs, seq_len, self.embedding_dim)
         ### END ASSIGN3_2
 
     
@@ -71,7 +77,17 @@ class Dropout(Module):
             output : Tensor of shape (*)
         """
         ### BEGIN ASSIGN3_2
-        raise NotImplementedError
+        if not self.training or self.p_dropout == 0.0:
+            return x
+
+        keep_prob = 1.0 - self.p_dropout
+        if keep_prob <= 0.0:
+            return zeros(x.shape, backend=x.backend)
+
+        mask_np = np.random.binomial(1, keep_prob, size=x.shape).astype(np.float32)
+        mask = tensor_from_numpy(mask_np, backend=x.backend)
+        scaled_mask = mask / keep_prob
+        return x * scaled_mask
         ### END ASSIGN3_2
 
 
@@ -91,7 +107,20 @@ class Linear(Module):
         """
         self.out_size = out_size
         ### BEGIN ASSIGN3_2
-        raise NotImplementedError
+        self.backend = backend
+        self.in_size = in_size
+        bound = 1.0 / math.sqrt(in_size)
+        weight_init = np.random.uniform(-bound, bound, size=(in_size, out_size)).astype(np.float32)
+        weight_tensor = tensor_from_numpy(weight_init, backend=backend, requires_grad=True)
+        self.weights = Parameter(weight_tensor)
+
+        self.use_bias = bias
+        if bias:
+            bias_init = np.random.uniform(-bound, bound, size=(out_size,)).astype(np.float32)
+            bias_tensor = tensor_from_numpy(bias_init, backend=backend, requires_grad=True)
+            self.bias = Parameter(bias_tensor)
+        else:
+            self.bias = None
         ### END ASSIGN3_2
 
     def forward(self, x: Tensor):
@@ -105,7 +134,10 @@ class Linear(Module):
         """
         batch, in_size = x.shape
         ### BEGIN ASSIGN3_2
-        raise NotImplementedError
+        result = x @ self.weights.value
+        if self.use_bias and self.bias is not None:
+            result = result + self.bias.value.view(1, self.out_size)
+        return result
         ### END ASSIGN3_2
 
 
@@ -125,7 +157,14 @@ class LayerNorm1d(Module):
         self.dim = dim
         self.eps = eps
         ### BEGIN ASSIGN3_2
-        raise NotImplementedError
+        self.backend = backend
+        weight_tensor = ones((dim,), backend=backend)
+        weight_tensor.requires_grad_(True)
+        self.weights = Parameter(weight_tensor)
+
+        bias_tensor = zeros((dim,), backend=backend)
+        bias_tensor.requires_grad_(True)
+        self.bias = Parameter(bias_tensor)
         ### END ASSIGN3_2
 
     def forward(self, x: Tensor) -> Tensor:
@@ -141,5 +180,12 @@ class LayerNorm1d(Module):
         """
         batch, dim = x.shape
         ### BEGIN ASSIGN3_2
-        raise NotImplementedError
+        mean = x.mean(dim=1)
+        variance = x.var(dim=1)
+        std = (variance + self.eps) ** 0.5
+        normalized = (x - mean) / std
+
+        weight = self.weights.value.view(1, self.dim)
+        bias = self.bias.value.view(1, self.dim)
+        return normalized * weight + bias
         ### END ASSIGN3_2
